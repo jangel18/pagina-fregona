bd1a9c619d9508689a061e97e9e6c2ee44345fc8
jangel18 <18jangel18@gmail.com>
2024-08-22 20:46:14 -0600

    Update registro_usuario.php
    prueba con codigo IA

--- Nueva carpeta/registro_usuario.php
+++ Nueva carpeta/registro_usuario.php
@@ -11,4 +11,13 @@
     
     $ejecutar= mysqli_query($connec,$query);
 
+    if ($ejecutar->execute()) {
+        // Redirigir a una página de éxito
+        header("Location: index3.php");
+        exit();
+    } else {
+        echo "Error: " . $ejecutar->error;
+    }
+
+
 ?>
