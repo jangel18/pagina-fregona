af5737eaafad3983b707f4ad9e797db674cf0ce4
jangel18 <18jangel18@gmail.com>
2024-08-27 22:09:21 -0600

    Jalo Perron
    EL Inicio de sesion ya es posible, ademas se agrego una funcion que mantiene la sesion iniciada

--- Nueva carpeta/Controllers/InicioDeSesion/Login_Usuario.php
+++ Nueva carpeta/Controllers/InicioDeSesion/Login_Usuario.php
@@ -1,13 +1,22 @@
 <?php 
-
+    session_start();
     include '../../Config/connection.php';
     $usuario= $_POST['Usuario'];
     $Contrasena= $_POST['Contrasena'];
-    $Validacion  =  "SELECT FROM usuarios WHERE Usuario ='$usuario' and Contrasena ='$Contrasena'";
-    $Respuesta = mysqli_query($connec,$Validacion);
+    // Construir la consulta SQL correctamente
+    $validacion = "SELECT * FROM usuarios WHERE Usuario = '$usuario' and Contrasena = '$Contrasena'";
 
-    if(!empty($Respuesta)){
-        echo" Error";
+    // Ejecutar la consulta
+    $respuesta = mysqli_query($connec, $validacion);
+
+    // Comprobar si se encontró un registro coincidente
+    if(mysqli_num_rows($respuesta) > 0) {
+        $usuarioDatos = mysqli_fetch_assoc($respuesta);
+        $_SESSION["Nombre"]=$usuarioDatos['Usuario'] ;
+        header("Location: ../../Vistas/Modulo-SesionInciada/SesionIniciada.php");
+        exit();
     }
-    else{ echo "Error: " . mysqli_error($connec);}
-?>
+    else{
+        echo "Error: Usuario o contraseña incorrectos.";
+    }
+ ?>
