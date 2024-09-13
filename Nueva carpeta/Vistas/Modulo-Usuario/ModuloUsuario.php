7a18ee9483b80adb07eaa2529b038e99164d3b00
jangel18 <18jangel18@gmail.com>
2024-09-12 23:08:05 -0600

    Inicio de clase Router

---
+++ Nueva carpeta/Vistas/Modulo-Usuario/ModuloUsuario.php
@@ -0,0 +1,21 @@
+<?php
+
+    session_start();
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+  <div>
+    <h3>Bienvenido Usuario <?php echo $_SESSION["Nombre"];?> </h3>
+  </div>
+</nav> 
+</body>
+</html>
